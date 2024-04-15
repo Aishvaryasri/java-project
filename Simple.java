9705e2a3d2ed0eb7259f9fd33bc855871c590cf6
Aishvaryasri <84492689+Aishvaryasri@users.noreply.github.com>
2024-04-15 20:04:17 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ class Simple {
       public static void main(String args[]){
 	      System.out.println("Welcome to Edureka Online Training !");
 	      	      System.out.println("Welcome !");
+	       			System.out.println("Welcome Aish !");
 
       }
 }
